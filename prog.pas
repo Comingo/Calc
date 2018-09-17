57f401880f3070c65d55f6eb4f2f5d893c5ccb97
Egor <40640834+Comingo@users.noreply.github.com>
2018-09-17 19:36:31 +0300

    Update prog.pas

--- prog.pas
+++ prog.pas
@@ -49,8 +49,40 @@ ClrScr;
     readln(c);
     writeln('Сумма A + B + C = ', a+b+c);
     writeln('Перезагрузите программу, чтобы выполнить еще операции.');
-    end
+    end;
 
+    
+    if zero = four then
+    begin
+    ClrScr;
+    writeln('Введите значение A:');
+    readln(a);
+    writeln('Введите значение B:');
+    readln(b);
+    writeln('Введите значение C:');
+    readln(c);
+    writeln('Введите значение D:');
+    readln(d);
+    writeln('Сумма A + B + C + D = ', a+b+c+d);
+    writeln('Перезагрузите программу, чтобы выполнить еще операции.');
+    end;
+
+    if zero = five then
+    begin
+    ClrScr;
+    writeln('Введите значение A:');
+    readln(a);
+    writeln('Введите значение B:');
+    readln(b);
+    writeln('Введите значение C:');
+    readln(c);
+    writeln('Введите значение D:');
+    readln(d);
+    writeln('Введите значение E:');
+    readln(e);
+    writeln('Сумма A + B + C + D + E = ', a+b+c+d+e);
+    writeln('Перезагрузите программу, чтобы выполнить еще операции.');
+    end;
 
 
 end;
